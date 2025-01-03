cb9d5a507e9c344e72f353170fe94b9a217bc5be
Karam Salama <karamslama244@gmail.com>
2025-01-04 00:09:53 +0200

    Add My Helper Classes

---
+++ lib/app/ecommerce_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Ecommerce extends StatelessWidget {
+  const Ecommerce({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
