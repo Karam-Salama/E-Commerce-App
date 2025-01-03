6c72a348d1dd502a485d881969527824f4bbdca9
Karam Salama <karamslama244@gmail.com>
2025-01-04 00:26:21 +0200

    make branches

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'app/ecommerce_app.dart';
+import 'core/cache/cache_helper.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  CacheHelper().init();
   runApp(const Ecommerce());
 }
